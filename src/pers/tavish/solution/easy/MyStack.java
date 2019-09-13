510f8c3ab85e42178f48672800d5cf7c0bdfeaee
Tavish <bit_zjm@hotmail.com>
2019-09-13 09:12:27 +0800

    Update Implementation

--- src/pers/tavish/solution/easy/MyStack.java
+++ src/pers/tavish/solution/easy/MyStack.java
@@ -15,26 +15,19 @@ public class MyStack {
     /** Push element x onto stack. */
     public void push(int x) {
         queue.add(x);
+        for (int i = 0; i < queue.size() - 1; i++) {
+            queue.add(queue.poll());
+        }
     }
 
     /** Removes the element on top of the stack and returns that element. */
     public int pop() {
-        int size = queue.size();
-        for (int i = 0; i < size - 1; i++) {
-            queue.add(queue.poll());
-        }
         return queue.poll();
     }
 
     /** Get the top element. */
     public int top() {
-        int size = queue.size();
-        for (int i = 0; i < size - 1; i++) {
-            queue.add(queue.poll());
-        }
-        int x = queue.peek();
-        queue.add(queue.poll());
-        return x;
+        return queue.peek();
     }
 
     /** Returns whether the stack is empty. */
